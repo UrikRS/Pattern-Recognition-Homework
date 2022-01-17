da422ef8b7a228ef6316bede49646c724d823989
urikRS <610521224@gms.ndhu.edu.tw>
2022-01-17 23:45:43 +0800

    ...

--- Homework2/homework2.py
+++ Homework2/homework2.py
@@ -5,6 +5,8 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix, classification_report
+from numpy.linalg import eig, pinv
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 def get_pgm(dir_name):
     f = open(dir_name, 'rb')
@@ -15,28 +17,38 @@ def get_pgm(dir_name):
     return img
 
 def get_data(dir_name):
-    data, label, test_d, test_l = [], [], [], []
+    x_train, y_train, x_test, y_test = [], [], [], []
     for i in range(1, 41):
         for j in range(1, 6):
-            data.append(get_pgm(dir_name + '\\s%d\\%d.pgm'%(i,j)).reshape(10304))
-            label.append(i)
+            x_train.append(get_pgm(dir_name + '\\s%d\\%d.pgm'%(i,j)).reshape(10304))
+            y_train.append(i)
     for i in range(1, 41):
         for j in range(6, 11):
-            test_d.append(get_pgm(dir_name + '\\s%d\\%d.pgm'%(i,j)).reshape(10304))
-            test_l.append(i)
-    return np.asarray(data), np.asarray(label), np.asarray(test_d), np.asarray(test_l)
+            x_test.append(get_pgm(dir_name + '\\s%d\\%d.pgm'%(i,j)).reshape(10304))
+            y_test.append(i)
+    return np.asarray(x_train), np.asarray(y_train), np.asarray(x_test), np.asarray(y_test)
 
 if __name__ == '__main__':
     x_train, y_train , x_test, y_test = get_data('Homework2\\att_faces')
     dimensions = [10, 20, 30, 40, 50]
-    sx_train, sx_test = StandardScaler().fit_transform(x_train), StandardScaler().fit_transform(x_test)
+    # 用 standard scaler
+    x_train, x_test = StandardScaler().fit_transform(x_train), StandardScaler().fit_transform(x_test)
+
     for dime in dimensions:
-        knn = KNeighborsClassifier(n_neighbors=3)
         pca = PCA(n_components=dime, random_state=1234)
-        rx_train, rx_test = pca.fit_transform(sx_train), pca.transform(sx_test)
+        rx_train, rx_test = pca.fit_transform(x_train), pca.transform(x_test)
+        knn = KNeighborsClassifier(n_neighbors=3)
         knn.fit(rx_train, y_train)
         predict = knn.predict(rx_test)
-        print(knn.score(rx_test, y_test))
         with np.printoptions(threshold=np.inf):
-            f = open('%dD.txt'%dime, 'w')
+            f = open('%dD_PCA.txt'%dime, 'w')
             f.write(str(confusion_matrix(y_test, predict))+'\n'+classification_report(y_test, predict))
+        lda = LinearDiscriminantAnalysis(n_components=dime, solver='eigen')
+        tx_train, tx_test = lda.fit_transform(rx_train, y_train), lda.transform(rx_test)
+        knn2 = KNeighborsClassifier(n_neighbors=3)
+        knn2.fit(tx_train, y_train)
+        predict2 = knn2.predict(tx_test)
+        print(knn2.score(tx_test, y_test))
+        with np.printoptions(threshold=np.inf):
+            f2 = open('%dD_FLD.txt'%dime, 'w')
+            f2.write(str(confusion_matrix(y_test, predict2))+'\n'+classification_report(y_test, predict2))
